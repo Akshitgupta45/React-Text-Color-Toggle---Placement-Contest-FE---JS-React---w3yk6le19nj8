15486d702d825135a8ba4515251c38e42e2d8da8
Akshitgupta45 <125752499+Akshitgupta45@users.noreply.github.com>
2023-10-01 13:28:27 +0000

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
-import React, {useEffect, useState} from 'react'
-import '../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
-//code here 
-  return (
-    <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
-    </div>
-  )
-}
-
+	//code here
+	const [classn, setClass] = useState("redColor");
+	function changeColor() {
+		if (classn == "redColor") {
+			setClass("blueColor");
+		} else {
+			setClass("redColor");
+		}
+	}
+	return (
+		<div id="main">
+			<p className={classn}>Newton School</p>
+			<button id="button" onClick={changeColor}>
+				Change Style
+			</button>
+		</div>
+	);
+};
 
 export default App;
